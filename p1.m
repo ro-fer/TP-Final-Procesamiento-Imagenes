5d418cb25c8d7fd8fd0090952d7a1662c178194d
Tomasmmunozburgos <tmunozburgos@estudiantes.unsam.edu.ar>
2024-06-18 22:31:43 -0300

    bordes_rojos.m

--- p1.m
+++ p1.m
@@ -1,69 +1,97 @@
-close all; clear; clc
-% leo una imagen y la convierto a escala de grises
-[file,dir] = uigetfile('*.bmp;*.jpg;*.png; *.tif'); %filtro para archivos bmp, jpg y png
-filename = [dir,file];
-info = imfinfo(filename);
 
-switch info.ColorType
-    case 'indexed'
-        [Iorig,map] = imread(filename);
-        I = ind2gray(Iorig,map); %convierto de indexada a escala de grises
-
-    case 'grayscale'
-        I = imread(filename);
+                if usadas(k, l)
+                    continue;
+                end
+                
+                % Verificar similitud con la pieza izquierda
+                if j > 1
+                    pieza_actual = imagen_reconstruida((i-1)*alto_pieza+1:i*alto_pieza, (j-2)*ancho_pieza+1:(j-1)*ancho_pieza, :);
+                    similitud = calcularSimilitud(pieza_actual, piezas_mezcladas{k, l}, 'horizontal');
+                    if similitud < mejor_similitud
+                        mejor_similitud = similitud;
+                        mejor_pieza = piezas_mezcladas{k, l};
+                        mejor_k = k;
+                        mejor_l = l;
+                    end
+                end
+                
+                % Verificar similitud con la pieza superior
+                if i > 1
+                    pieza_actual = imagen_reconstruida((i-2)*alto_pieza+1:(i-1)*alto_pieza, (j-1)*ancho_pieza+1:j*ancho_pieza, :);
+                    similitud = calcularSimilitud(pieza_actual, piezas_mezcladas{k, l}, 'vertical');
+                    if similitud < mejor_similitud
+                        mejor_similitud = similitud;
+                        mejor_pieza = piezas_mezcladas{k, l};
+                        mejor_k = k;
+                        mejor_l = l;
+                    end
+                end
+                
+                % Verificar similitud con la pieza derecha
+                if j < 4
+                    pieza_actual = imagen_reconstruida((i-1)*alto_pieza+1:i*alto_pieza, j*ancho_pieza+1:(j+1)*ancho_pieza, :);
+                    similitud = calcularSimilitud(pieza_actual, piezas_mezcladas{k, l}, 'horizontal');
+                    if similitud < mejor_similitud
+                        mejor_similitud = similitud;
+                        mejor_pieza = piezas_mezcladas{k, l};
+                        mejor_k = k;
+                        mejor_l = l;
+                    end
+                end
+                
+                % Verificar similitud con la pieza inferior
+                if i < 4
+                    pieza_actual = imagen_reconstruida(i*alto_pieza+1:(i+1)*alto_pieza, (j-1)*ancho_pieza+1:j*ancho_pieza, :);
+                    similitud = calcularSimilitud(pieza_actual, piezas_mezcladas{k, l}, 'vertical');
+                    if similitud < mejor_similitud
+                        mejor_similitud = similitud;
+                        mejor_pieza = piezas_mezcladas{k, l};
+                        mejor_k = k;
+                        mejor_l = l;
+                    end
+                end
+            end
+        end
         
-    case 'truecolor'
-        Iorig = imread(filename);
-        I = rgb2gray(Iorig); %convierto de RGB a escala de grises       
+        % Colocar la mejor pieza encontrada en la posición correcta
+        fila_inicio = (i-1) * alto_pieza + 1;
+        fila_fin = i * alto_pieza;
+        col_inicio = (j-1) * ancho_pieza + 1;
+        col_fin = j * ancho_pieza;
+
+        imagen_reconstruida(fila_inicio:fila_fin, col_inicio:col_fin, :) = mejor_pieza;
+        
+        % Marcar la pieza como usada
+        usadas(mejor_k, mejor_l) = true;
+    end
 end
 
-I = im2double(I);
+% Convertir la imagen reconstruida de L*a*b* a RGB
+imagen_reconstruida_rgb = lab2rgb(imagen_reconstruida);
 
-Hx=fspecial('sobel'); %S
-Hy=rot90(Hx); %E 
-Hx1=rot90(Hx,2);
-Hy1=rot90(Hx,3);
-
-% Gradiente en x e y usando filtro Sobel
-Gx = imfilter(double(I), Hx);
-Gy = imfilter(double(I), Hy);
-
-% Gradiente en x e y usando filtro Sobel
-Gx1 = imfilter(double(I), Hx1);
-Gy1 = imfilter(double(I), Hy1);
-
-magnitude = sqrt(Gx.^2 + Gy.^2);
-angle = atan2d(Gy, Gx); % ángulo en grados
-
-magnitude1 = sqrt(Gx1.^2 + Gy1.^2);
-angle1 = atan2d(Gy1, Gx1); % ángulo en grados
-
-target_angle = 4.5; % ejemplo: 45 grados
-tolerance = 2; % tolerancia de ±10 grados
-
-% Máscara para seleccionar bordes en el rango de ángulos
-mask = (angle >= target_angle - tolerance) & (angle <= target_angle + tolerance);
-
-% Máscara para seleccionar bordes en el rango de ángulos
-mask1 = (angle1 >= target_angle - tolerance) & (angle1 <= target_angle + tolerance);
-
-% Aplicar la máscara a la magnitud del gradiente
-selected_edges = magnitude .* mask;
-
-% Aplicar la máscara a la magnitud del gradiente
-selected_edges1 = magnitude1 .* mask1;
-
-O = selected_edges + selected_edges1;
-
+% Mostrar la imagen original, la imagen desordenada y la imagen reconstruida
 figure;
-subplot(3,1,1)
-imshow(selected_edges, []);
-title(['Bordes en ángulo de ', num2str(target_angle), ' ± ', num2str(tolerance), ' grados']);
+subplot(1, 3, 1);
+imshow(imagenRGB);
+title('Imagen Original');
 
-subplot(3,1,2)
-imshow(selected_edges1, []);
-title(['Bordes en ángulo de ', num2str(target_angle), ' ± ', num2str(tolerance), ' grados']);
+subplot(1, 3, 2);
+imshow(imagen_desordenada_rgb);
+title('Imagen Desordenada');
 
-subplot(3,1,3)
-imshow(O, []);
-title(['Imagen final']);
+subplot(1, 3, 3);
+imshow(imagen_reconstruida_rgb);
+title('Imagen Reconstruida');
+
+% Función para calcular la similitud entre los bordes de dos piezas
+function similarity = calcularSimilitud(pieza1, pieza2, direccion)
+    if strcmp(direccion, 'horizontal')
+        borde1 = pieza1(:, end, :);
+        borde2 = pieza2(:, 1, :);
+    elseif strcmp(direccion, 'vertical')
+        borde1 = pieza1(end, :, :);
+        borde2 = pieza2(1, :, :);
+    end
+    diferencia = borde1 - borde2;
+    similarity = sum(diferencia(:).^2);
+end
